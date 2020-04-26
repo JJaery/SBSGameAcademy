78855224391ffff52c0a6db6f9cb1df55969ff7c
전인재 <aa@aa.com>
2020-04-26 13:38:56 +0900

    플레이어 프리팹 프로젝트 푸시

---
+++ PlayerPrefs/New Unity Project/Assets/Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public Text text;
+    float exp = 0;
+    public TestData data = new TestData();
+
+    void Start()
+    {
+        var json = PlayerPrefs.GetString("Account/charData222", "");
+        if (string.IsNullOrEmpty(json) == false)
+        {
+            data = JsonUtility.FromJson<TestData>(json);
+        }
+        else
+        {
+            data.name = "나는 바보다";
+        }
+        Debug.Log(data.name);
+    }
+    private void Update()
+    {
+        data.exp += 0.01f;
+        text.text = data.exp.ToString();
+        data.name = "나는 바보가 아니다";
+    }
+
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetString("Account/charData222", JsonUtility.ToJson(data));
+        PlayerPrefs.Save();
+    }
+}
+
+
+public class TestData
+{
+    public string name = "";
+    public float exp = 0f;
+}
