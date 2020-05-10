f8e575368d19340d28d01b65c87a17974ded9575
전인재 <aa@aa.com>
2020-05-10 14:19:01 +0900

    스팀웍스 테스트

---
+++ Steamworks/SteamWorksTest/Assets/Plugins/SteamTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Steamworks;
+
+
+public class SteamTest : MonoBehaviour
+{
+    [SerializeField]
+    Text _uiText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SteamManager.Initialized)
+        {
+            string name = SteamFriends.GetPersonaName();
+            SteamFriends.ActivateGameOverlay("Players");
+            _uiText.text = name;
+        }
+        else
+            _uiText.text = "Failed";
+    }
+}
